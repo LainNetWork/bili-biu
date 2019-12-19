812d65192debfba2cddccb9010c38e4bdfd4c4f0
Lain <tianshang360@163.com>
2019-12-19 18:27:26 +0800

    前端页面调整,提交代码

--- src/main/java/fun/lain/bilibiu/web/entity/SaveCollection.java
+++ src/main/java/fun/lain/bilibiu/web/entity/SaveCollection.java
@@ -3,10 +3,7 @@ package fun.lain.bilibiu.web.entity;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 
 /**
@@ -17,6 +14,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 @TableName("save_collect")
 public class SaveCollection {
     @TableId(type = IdType.AUTO)
--- src/main/java/fun/lain/bilibiu/web/service/impl/MonitorTask.java
+++ src/main/java/fun/lain/bilibiu/web/service/impl/MonitorTask.java
@@ -1,18 +1,29 @@
 package fun.lain.bilibiu.web.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import fun.lain.bilibiu.collection.service.ApiService;
+import fun.lain.bilibiu.common.BeanUtil;
+import fun.lain.bilibiu.web.entity.SaveCollection;
 import fun.lain.bilibiu.web.entity.SaveTask;
+import fun.lain.bilibiu.web.mapper.SaveCollectionMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
+import java.util.List;
+
+
 @Slf4j
 public class MonitorTask implements Job {
     @Override
     public void execute(JobExecutionContext context) throws JobExecutionException {
+        //获取需要的SpringBean
+        SaveCollectionMapper saveCollectionDao = BeanUtil.getBean(SaveCollectionMapper.class);
         SaveTask task = (SaveTask) context.getMergedJobDataMap().get("task");
         log.info("进入定时任务！");
-        log.info("定时任务内容：");
-        System.out.println(task);
+        //获取此任务中用户要监控的收藏夹信息
+        List<SaveCollection> list = saveCollectionDao.selectList(new QueryWrapper<SaveCollection>().eq("taskId",task.getId()));
+        System.out.println(list);
     }
 }
